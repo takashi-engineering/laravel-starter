56c193867a78bd996cf171819bd33255f088217f
takashi <pero857942632@gmail.com>
2023-06-10 19:31:45 +0900

    つぶやきの更新処理を実装

--- app/Http/Requests/Tweet/UpdateRequest.php
+++ app/Http/Requests/Tweet/UpdateRequest.php
@@ -31,4 +31,8 @@ class UpdateRequest extends FormRequest
     {
         return $this->input('tweet');
     }
+
+    public function id(): int {
+        return (int) $this->route('tweetId');
+    }
 }
